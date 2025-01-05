d3ac97c1e6bac9b381fec10739e900750901c353
Oliyad Diriba <oliyaddiriba75@gmail.com>
2025-01-05 13:42:16 +0300

    Add function for choose file for cv

--- JOB IN/ApplicantSignUp.cs
+++ JOB IN/ApplicantSignUp.cs
@@ -24,5 +24,32 @@ namespace JOB_IN
             mainPanel1.Controls.Add(infoPanel2);
         }
 
+        private void onUploadBtn_clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void OnChooseFile_clicked(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Title = "Select a File";
+                openFileDialog.Filter = "All Files (*.*)|*.*";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog.FileName;
+
+                    MessageBox.Show("Selected file: " + filePath);
+                    cvField.Text = filePath;
+                }
+            }
+        }
+
+        private void OnPlusSign_clicked(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
